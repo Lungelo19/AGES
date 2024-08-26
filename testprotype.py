b071a414e71d2219eda3d44e543a0d1ef7cc9229
Lungelo19 <sbongakonkelungelo69@gmail.com>
2024-08-26 22:46:34 +0200

    aug 26 22:46

--- testprotype.py
+++ testprotype.py
@@ -3,7 +3,7 @@ from Data_Source import Courses
 from valuator import Validator
 
 
-student1 = Student("applied_maths&compt.xlsx","failed_agre")
+student1 = Student("botany&microbiology.xlsx",1)
 stu_status = Validator(student1,Courses)
 
 print("\n")
